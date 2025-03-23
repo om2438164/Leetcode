ae7659968ec1d44cac4bb4ec60480fcd928cc61a
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-03-23 19:29:38 +0530

    Time: 106 ms (5.77%), Space: 96 MB (10.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,9 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans(nums1);
-        for(auto it:nums2) ans.push_back(it);
-        sort(ans.begin(),ans.end());
+        // vector<int>ans(nums1);
+        // for(auto it:nums2) ans.push_back(it);
+        // sort(ans.begin(),ans.end());
+        vector<int>ans;
+        int first=0,second=0;
+        while(first<nums1.size() && second<nums2.size()){
+            if(nums1[first]<nums2[second]){
+                ans.push_back(nums1[first]);
+                first++;
+            } 
+            else if(nums1[first]>nums2[second]){
+                ans.push_back(nums2[second]);
+                second++;
+            }
+            else{
+                ans.push_back(nums1[first]);
+                ans.push_back(nums2[second]);
+                first++,second++;
+            }
+        }
+
+        while(first<nums1.size()){
+            ans.push_back(nums1[first]);
+                first++;
+        }
+        while(second<nums2.size()){
+            ans.push_back(nums2[second]);
+                second++;
+        }
+
+        for(auto it:ans) cout<<it<<endl;
+
+
         if(ans.size()%2!=0) return ans[ans.size()/2];
         int sample=ans.size()/2;
         double temp=double (ans[sample]+ans[sample-1])/2;
