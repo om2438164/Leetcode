b3b9ddb7d06a682749afbe5706480778a3f3e4cd
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-03-28 10:38:38 +0530

    Time: 171 ms (5.37%), Space: 137.6 MB (7.11%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,20 +3,16 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> ans(n, 0);
-        stack<vector<int>> st; // Stack stores {index, temperature}
-
+        stack<vector<int>> st; 
         for (int i = n - 1; i >= 0; i--) {
-            // Pop elements until we find a warmer temperature
             while (!st.empty() && st.top()[1] <= temperatures[i]) {
                 st.pop();
             }
             
-            // If stack is not empty, calculate days to the next warmer temperature
             if (!st.empty()) {
                 ans[i] = st.top()[0] - i;
             }
 
-            // Push current element {index, temperature} into stack
             st.push({i, temperatures[i]});
         }
 
