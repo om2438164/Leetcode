f52d876f5aa259b86bee593e9c00eacc82a0d111
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-03-31 22:59:31 +0530

    Time: 17 ms (8.62%), Space: 21.2 MB (5.71%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        map<int,int>freq;
+        for(auto it:nums) freq[it]++;
+        int index=0;
+        int count=0;
+        for(auto it:freq){
+            if(it.second>=2){
+                nums[index]=it.first;
+                index++;
+                nums[index]=it.first;
+                count+=2;
+            }else{
+                nums[index]=it.first;
+                count++;
+            }
+            index++;
+        }
+        return count;
+    }
+};
