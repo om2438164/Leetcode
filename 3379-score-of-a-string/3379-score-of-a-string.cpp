97800e7e8ef0494ad11e6ed68495495c254c9b58
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-04 22:56:43 +0530

    Time: 15 ms (1.42%), Space: 9 MB (28.26%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans=0;
+        
+        for(int i=0;i<s.size()-1;i++){
+            ans= ans + abs(s[i]-s[i+1]);
+            cout<<ans<<endl;
+        }
+        return ans;
+    }
+};
