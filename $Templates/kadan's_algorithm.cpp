33620f41fbff1c82cc1279894e01437eac14e7b9
ompatil <om2438164@gmail.com>
2025-04-08 20:41:59 +0530

    kadan's algorithm added

---
+++ $Templates/kadan's_algorithm.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            //kadan's algorithm
+    
+            int currsum=0,maxi=INT_MIN;
+            for(int i=0;i<nums.size();i++){
+                currsum+=nums[i];
+                maxi=max(currsum,maxi);
+                if(currsum<0) currsum=0;
+    
+            }
+            return maxi;
+        }
+    };
