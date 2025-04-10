467ac0f371dfc82430810f17204a2e80d098cee8
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-10 20:14:50 +0530

    Time: 37 ms (83.39%), Space: 46.8 MB (40.08%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -18,19 +18,43 @@ public:
         //     j++;
         // }
         // return count;
-        int count=0;
-        for(int i=0;i<nums.size()-1;i++){
-            int sum=nums[i];
-            if(nums[i]==k) count++;
-            for(int j=i+1;j<nums.size();j++){
-                sum+=nums[j];
-                if(sum==k) count++;
-            }
-        }
-        if(nums[nums.size()-1]==k) count++;
-        return count;
 
+
+        // brute force solution
+        // int count=0;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     int sum=nums[i];
+        //     if(nums[i]==k) count++;
+        //     for(int j=i+1;j<nums.size();j++){
+        //         sum+=nums[j];
+        //         if(sum==k) count++;
+        //     }
+        // }
+        // if(nums[nums.size()-1]==k) count++;
+        // return count;
+
+
+        // using prefix sum pattern
+
+        vector<int>prefix(nums.size());
+        unordered_map<int,int>map;
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        int j=0,ans=0;
+        while(j<nums.size()){
+            if(prefix[j]==k) ans++;
+            int temp=prefix[j]-k;
+            if(map.count(temp)){
+                ans+=map[temp];
+            }
+            map[prefix[j]]++;
+            j++;
+        }
+
+        return ans;
         
-        
+         
     }
 };
