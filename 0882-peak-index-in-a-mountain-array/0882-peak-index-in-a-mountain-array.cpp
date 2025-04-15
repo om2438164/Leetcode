647e69e879d0bee9e92c9d3cf8b3e35df7a3c9fa
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-15 23:46:24 +0530

    Time: 0 ms (100%), Space: 63.4 MB (78.82%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -4,10 +4,11 @@ public:
         int start = 0, end = arr.size() - 1;
         while (start < end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] < arr[mid + 1]) {
-                start = mid + 1;
-            } else {
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) return mid;
+            else if (arr[mid] > arr[mid + 1]) {
                 end = mid;
+            } else {
+                start = mid+1;
             }
         }
         return start;
