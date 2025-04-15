47449df35c48db51c3fa51a3ebc0054d367e43cf
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-15 21:02:12 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.19%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int start=0,end=nums.size()-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==target)return mid;
+            if(nums[mid]==target) return mid;
             else if(nums[mid]<target) start=mid+1;
             else end=mid-1;
         }
