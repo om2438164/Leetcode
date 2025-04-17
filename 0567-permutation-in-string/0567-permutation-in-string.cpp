d4d95ecad1493031ef8c325fb5bdd539e35ea203
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-17 14:45:00 +0530

    Time: 3 ms (66.8%), Space: 10.7 MB (26.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    bool getsum(string& arr, string& hell, int k) {
-        if (k > arr.size()) return false; // Ensure k is valid
-
-        unordered_map<char, int> map, temp;
-        
-        for (char ch : hell) map[ch]++;
-        
-        int i = 0, j = 0;
-        
-        while (j < k) {
-            temp[arr[j]]++;
-            j++;
-        }
-        
-        if (temp == map) return true; // Check for first window
-
-        while (j < arr.size()) {  
-            temp[arr[j]]++;  // Add new character
-            temp[arr[i]]--;  // Remove old character
-
-            if (temp[arr[i]] == 0) temp.erase(arr[i]); // Erase zero-frequency char
-            
-            if (temp == map) return true; // Check current window
-
-            j++;
-            i++;
-        }
-        
-        return false;
-    }
-
     bool checkInclusion(string s1, string s2) {
-        return getsum(s2, s1, s1.size());
+
+        // Brute force 
+        // unordered_map<char,int>map1;
+        // unordered_map<char,int>map2;
+        // int k=s1.size();
+        // for(auto it:s1) map1[it]++;
+        // for(int i=0;i<=s2.size()-k;i++){
+        //     for(int j=i;j<i+k;j++){
+        //         map2[s2[j]]++;
+        //     }
+        //     if(map1==map2) return true;
+        //     map2.clear();
+        // }
+        // return false;
+        
+        unordered_map<char,int>map1;
+        unordered_map<char,int>map2;
+        int k=s1.size();
+        if (k > s2.size()) return false;
+        for(auto it:s1) map1[it]++;
+        int j=0;
+        while(j<k){
+            map2[s2[j]]++;
+            j++;
+        }
+        int i=0;
+        while(j<s2.size()){
+            if(map1==map2) return true;
+            map2[s2[j]]++;
+            map2[s2[i]]--;
+            if(map2[s2[i]]==0) map2.erase(s2[i]);
+            i++,j++;
+        }
+        if(map1==map2) return true;
+        return false;
     }
 };
