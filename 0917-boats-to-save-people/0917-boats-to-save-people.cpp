7b663b3fca97beb3d7a307053464be36b1dfdca8
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-21 23:11:50 +0530

    Time: 11 ms (91.53%), Space: 45.9 MB (37.8%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.cpp
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        // sort(people.begin(),people.end());
-        // int i=0,j=1;
-        // int count=0;
-        // while(j<people.size()){
-        //     int sum=people[i]+people[j];
-        //     if(sum>limit){
-        //         count++;
-        //         i++,j++;
-        //     }else{
-        //         count++;
-        //         i=i+2;
-        //         j=j+2;
-        //     }
-        // }
-        // if(i==people.size()-1) count++;
-        // return count;
-
         // tow pointer
         sort(people.begin(),people.end());
         int i=0,j=people.size()-1;
         int count=0;
         while(i<j){
             int sum=people[i]+people[j];
-            
             if(sum<=limit) {
                 count++;
                 i++,j--;
@@ -36,10 +18,8 @@ public:
                 else{
                     j--;
                     count++;
-                }
-                
+                }   
             }
-            
 
         }
         if(i==j) count++;
