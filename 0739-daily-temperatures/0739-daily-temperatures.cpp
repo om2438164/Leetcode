22b609d77a92863b9a5da083d9e5633545e69e48
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-24 11:19:28 +0530

    Time: 11 ms (94.04%), Space: 102.8 MB (81.27%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -19,17 +19,17 @@ public:
         // using stack -----------
         int n = temperatures.size();
         vector<int> ans(n, 0);
-        stack<vector<int>> st; 
+        stack<int>st; 
         for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && st.top()[1] <= temperatures[i]) {
+            while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
                 st.pop();
             }
             
             if (!st.empty()) {
-                ans[i] = st.top()[0] - i;
+                ans[i] = st.top() - i;
             }
 
-            st.push({i, temperatures[i]});
+            st.push(i);
         }
 
         return ans;
