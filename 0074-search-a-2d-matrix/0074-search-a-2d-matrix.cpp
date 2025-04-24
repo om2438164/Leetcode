a06102b423cb6e30d5f12215fc1371b75151650e
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-24 22:56:34 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.22%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(auto it:matrix){
-        
-                if(binary_search(it.begin(),it.end(),target)){
-                    return true;
-                }
-            
-            
-        } 
+        // for(auto it:matrix){
+        //         if(binary_search(it.begin(),it.end(),target)){
+        //             return true;
+        //         }
+        // } 
+        // return false;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int start=0,end=row*col-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int element=matrix[mid/col][mid%col];
+            if(element==target) return true;
+            else if(element<target) start=mid+1;
+            else end=mid-1;
+        }
         return false;
     }
 };
