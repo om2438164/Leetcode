cb17ab6ae13995b5f241c69ceb7956d59e1aa356
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-24 10:43:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x,0.5);
+        // return pow(x,0.5);
+        // sqrt(x)
+
+        if(x==0 || x==1) return x;
+        int start=1,end=x/2;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid==x/mid) return mid;
+            else if(mid<(x/mid)){
+                ans=mid;
+                start=mid+1;
+            }else end=mid-1;
+        }
+        return ans;
     }
 };
