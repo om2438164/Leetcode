1877c1833e9e8fd53816cb968aee54d0f6f8f9f8
ompatil <om2438164@gmail.com>
2025-04-27 22:18:47 +0530

    commited

--- $Templates/Binary_search.cpp
+++ $Templates/Binary_search.cpp
@@ -11,3 +11,48 @@ int binarySearch(vector<int>& nums, int target) {
     
     return -1; // Target not found
 }
+
+
+// Binary search standard problem pages maximum of minimum or painter problem 
+
+class Solution {
+    public:
+    
+        bool check(vector<int>&weights,int mid,int k){
+            int currsum=0;
+            for(int i=0;i<weights.size();i++){
+                if(weights[i]>mid) return false;
+                currsum+=weights[i];
+                if(currsum>mid){
+                    k--;
+                    currsum=weights[i];
+                    if(k<=0) return false;
+                }
+                
+            }
+           
+            return true;
+        }
+    
+    
+        int shipWithinDays(vector<int>& weights, int days) {
+            int sum=0,start=INT_MIN;
+            for(auto it:weights) {
+                sum+=it;
+                start=max(start,it);
+            }
+    
+            int end=sum;
+            int ans=0;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(check(weights,mid,days)){
+                    ans=mid;
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+            return ans;
+        }
+    };
