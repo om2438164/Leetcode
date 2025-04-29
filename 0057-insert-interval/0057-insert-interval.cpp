d9fb03a3d552813a3905000230610a67944f3809
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-04-29 10:56:47 +0530

    Time: 6 ms (10.77%), Space: 23.7 MB (5.38%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
 
-        intervals.push_back({newInterval[0],newInterval[1]});
-        
-        sort(intervals.begin(),intervals.end());
+        // intervals.push_back({newInterval[0],newInterval[1]});
 
-        vector<vector<int>>ans;
-        for(auto it:intervals){
-            if(ans.empty() || it[0]>ans.back()[1]){
+        // sort(intervals.begin(),intervals.end());
+
+        // vector<vector<int>>ans;
+        // for(auto it:intervals){
+        //     if(ans.empty() || it[0]>ans.back()[1]){
+        //         ans.push_back(it);
+        //     }else{
+        //         //overlapping
+        //         int temp=max(it[1],ans.back()[1]);
+        //         ans.back()[1]=temp;
+        //     }
+        // }
+        // return ans;
+        vector<vector<int>> temp;
+        vector<vector<int>> ans;
+        int i = 0;
+        while (i < intervals.size() && intervals[i][0] <= newInterval[0] ) {
+            temp.push_back(intervals[i]);
+            i++;
+        }
+        temp.push_back(newInterval);
+        while (i < intervals.size()) {
+            temp.push_back(intervals[i]);
+            i++;
+        }
+
+        for (auto it : temp) {
+            if (ans.empty() || it[0] > ans.back()[1]) {
                 ans.push_back(it);
-            }else{
-                //overlapping
-                int temp=max(it[1],ans.back()[1]);
-                ans.back()[1]=temp;
+            } else {
+                // overlapping
+                ans.back()[1] = max(it[1], ans.back()[1]);
             }
         }
         return ans;
