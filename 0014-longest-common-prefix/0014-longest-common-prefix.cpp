d61001880d2bb95dfc950dc4aaab02cf8eb91322
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-01 20:21:32 +0530

    Time: 0 ms (100%), Space: 12 MB (35.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans=strs[0];
+        // string ans=strs[0];
+        // for(int i=1;i<strs.size();i++){
+        //     if(strs[i].size()<ans.size()){
+        //         ans=strs[i];
+        //     }
+        // }
+
+        // for(auto it:strs){
+        //     string temp="";
+        //     for(int i=0;i<it.size() && i<ans.size() ;i++){
+
+        //         if(it[i]==ans[i]){
+        //             temp.push_back(it[i]);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        //     ans=temp;
+        // }
+
+       string ans=strs[0];
+       
         for(int i=1;i<strs.size();i++){
-            if(strs[i].size()<ans.size()){
-                ans=strs[i];
+            string result="";
+            for(int j=0;j<strs[i].size() && j<ans.size();j++){
+                if(ans[j]==strs[i][j]){
+                    result.push_back(ans[j]);
+                }else break;
+                
             }
-        }
-        
-        for(auto it:strs){
-            string temp="";
-            for(int i=0;i<it.size() && i<ans.size() ;i++){
-               
-                if(it[i]==ans[i]){
-                    temp.push_back(it[i]);
-                }else{
-                    break;
-                }
-            }
-            ans=temp;
+            ans=result;
         }
 
         return ans;
