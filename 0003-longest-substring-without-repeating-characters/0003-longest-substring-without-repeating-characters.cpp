e5066ed746e6e7a2cd375963e3e24021595c7890
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-02 13:01:09 +0530

    Time: 12 ms (46.57%), Space: 12.3 MB (43.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, bool> map;
+        unordered_map<char,int>map;
+        int i = 0, j = 0, ans = 0;
 
-        int i = 0, j = 0;
-        int ans=0;
         while (j < s.size()) {
+            // do logic here to add arr[right] to curr
             // expand the window
-            if (!map.count(s[j])) {
-                map[s[j]] = true;
-                // cout<<s[j]<<"\n";
-                ans=max(ans,j-i+1);
-                j++;
-            } else {
-                while (map.count(s[j])) {
-                    // // shrink the window
-                    // cout<<"erase";
-                    // cout<<s[i]<<"/n";
-                    map.erase(s[i]);
-                    i++;
-                }
-               
-                ans=max(ans,j-i);
-            }
-            
+            map[s[j]]++;
 
+            while (map[s[j]]>1) {
+                // remove arr[left] from curr or
+                // shrink the window
+                map[s[i]]--;
+                if(map[s[i]]==0) map.erase(s[i]);
+                i++;
+            }
+
+            // update ans
+            ans=max(ans,j-i+1);
+            j++;
         }
+
         return ans;
     }
 };
