6bfc8434e12e632e78a6dbaef7d2724a3ccf3cb6
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-04 01:03:48 +0530

    Time: 7 ms (3.76%), Space: 26.4 MB (9.1%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         unordered_map<int,int>map;
         int sample=nums.size()/3;
-        set<int>s;
         vector<int>ans;
         for(auto it:nums){
             map[it]++;
-            if(map[it]>sample){
-                s.insert(it);
+        }
+        for(auto it:map){
+            if(it.second>sample){
+                ans.push_back(it.first);
             }
         }
-        for(auto it:s) ans.push_back(it);
         return ans;
     }
 };
