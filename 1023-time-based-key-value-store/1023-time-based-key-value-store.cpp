22d62d5a9852a13143297de73ccd5d1a42d82b13
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-05 19:23:38 +0530

    Time: 82 ms (14.71%), Space: 145.2 MB (6.37%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -0,0 +1,72 @@
+class TimeMap {
+public:
+    // unordered_map<string,string>map;
+    // TimeMap() {
+
+    // }
+
+    // void set(string key, string value, int timestamp) {
+    //     string temp=key+'#'+to_string(timestamp);
+    //     map[temp]=value;
+    // }
+
+    // string get(string key, int timestamp) {
+    //     string temp=key+'#'+to_string(timestamp);
+    //     if(map.count(temp)){
+    //         return map[temp];
+    //     }else{
+    //         for(int i=timestamp;i>=1;i--){
+    //             string temp=key+'#'+to_string(i);
+    //             if(map.count(temp)) return map[temp];
+
+    //         }
+
+    //     }
+    //     return "";
+
+    unordered_map<string, string> map;
+    unordered_map<string,vector<int>>map2;
+    TimeMap() {
+
+    }
+
+    void set(string key, string value, int timestamp) {
+        string temp=key+'#'+to_string(timestamp);
+        map[temp]=value;
+        map2[key].push_back(timestamp);
+    }
+
+    string get(string key, int timestamp) {
+        string temp=key+'#'+to_string(timestamp);
+        if(map.count(temp)){
+            return map[temp];
+        }else{
+            vector<int>&v=map2[key];
+            int start=0,end=v.size()-1;
+            int ans=-1;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(v[mid]<timestamp){
+                    ans=v[mid];
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+            if (ans != -1) {
+                string temp = key + '#' + to_string(ans);
+                return map[temp];
+            }
+
+        }
+        return "";
+}
+}
+;
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
