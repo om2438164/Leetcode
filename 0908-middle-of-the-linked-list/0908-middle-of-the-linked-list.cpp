cefb77f93ebbe49b97e72c6dd560046fc6fca3ab
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-07 17:48:06 +0530

    Time: 0 ms (100%), Space: 9.9 MB (89.28%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -10,38 +10,38 @@
  */
 class Solution {
 public:
-    int getlen(ListNode* &head){
-        ListNode* temp=head;
-        int count=0;
-        while(temp!=NULL){
-            temp=temp->next;
-            count++;
-        }
-        return count;
-    }
+    // int getlen(ListNode* &head){
+    //     ListNode* temp=head;
+    //     int count=0;
+    //     while(temp!=NULL){
+    //         temp=temp->next;
+    //         count++;
+    //     }
+    //     return count;
+    // }
 
     ListNode* middleNode(ListNode* head) {
         // using double traversal, finding length and ans
-        int len=getlen(head);
-        int pos=(len/2)+1;
+        // int len=getlen(head);
+        // int pos=(len/2)+1;
 
-        ListNode* temp=head;
-        while(pos!=1){
-            temp=temp->next;
-            pos--;
-        }
-        return temp;
+        // ListNode* temp=head;
+        // while(pos!=1){
+        //     temp=temp->next;
+        //     pos--;
+        // }
+        // return temp;
 
 
 
 
         // using fast and slow pointer
-        // ListNode* slow=head;
-        // ListNode* fast=head;
-        // while(fast!=NULL && fast->next!=NULL){
-        //     fast=fast->next->next;
-        //     slow=slow->next;
-        // }
-        // return slow;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
     }
 };
