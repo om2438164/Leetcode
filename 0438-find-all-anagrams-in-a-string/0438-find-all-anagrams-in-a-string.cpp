329f34148e568d5f2cc673095fb62fa87a8a0e65
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-07 14:30:33 +0530

    Time: 28 ms (21.48%), Space: 14.6 MB (27.52%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,int>map;
-        unordered_map<char,int>temp;
-        vector<int>ans;
-        int k=p.size();
-        for(auto it:p) map[it]++;
-        int i=0,j=0;
-        while(j<k){
+        // brute force
+
+        // int n=p.size();
+        // unordered_map<char,int>map;
+        // vector<int>ans;
+        // for(auto it:p) map[it]++;
+        // for(int i=0;i<=s.size()-n;i++){
+        //     unordered_map<char,int>temp;
+        //     for(int j=i;j<i+n;j++){
+        //         temp[s[j]]++;
+        //     }
+        //     if(map==temp){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        unordered_map<char, int> map;
+        unordered_map<char, int> temp;
+        vector<int> ans;
+        for (auto it : p)
+            map[it]++;
+        int j = 0, i = 0;
+        int k = p.size();
+        while (j < k) {
             temp[s[j]]++;
             j++;
         }
-        while(j<=s.size()){
-            if(map==temp) ans.push_back(i);
-            temp[s[i]]--;
-            if(temp[s[i]]==0) temp.erase(s[i]);
+        // validate first window
+        if (map == temp) {
+            ans.push_back(i);
+        }
+
+        while (j < s.size()) {
             temp[s[j]]++;
-            j++,i++;
+            temp[s[i]]--;
+            if (temp[s[i]] == 0)
+                temp.erase(s[i]);
+            i++, j++;
+            if (map == temp) {
+                ans.push_back(i);
+            }
         }
         return ans;
-
-
     }
 };
