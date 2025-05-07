d40b2260d692c9e101581ce16fa4f0c951d0a091
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-07 13:31:18 +0530

    Time: 203 ms (9.37%), Space: 53.8 MB (7.95%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        // brute force 
+        // set<vector<int>>temp;
+        // vector<vector<int>>ans;
+        // for(int i=0;nums.size()>=4 && i<nums.size()-3;i++){
+        //     for(int j=i+1;j<nums.size()-2;j++){
+        //         for(int k=j+1;k<nums.size()-1;k++){
+        //             for(int l=k+1;l<nums.size();l++){
+        //                 long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
+        //                 if(sum==target){
+        //                     vector<int>sample={nums[i],nums[j],nums[k],nums[l]};
+        //                     sort(sample.begin(),sample.end());
+        //                     temp.insert(sample);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // for(auto it:temp) ans.push_back(it);
+        // return ans;
+
+        // using two pointers 
         set<vector<int>>temp;
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
