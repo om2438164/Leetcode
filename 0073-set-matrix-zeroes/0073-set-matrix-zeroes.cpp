f65a7183dad0b855ea77412e4ba399a99ef0ad8f
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-12 21:18:58 +0530

    Time: 0 ms (100%), Space: 18.8 MB (12.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> row;
-        vector<int> col;
+        unordered_map<int,int>row;
+        unordered_map<int,int>col;
 
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[i].size(); j++) {
                 if (matrix[i][j] == 0) {
-                    row.push_back(i);
-                    break;
+                    if(!row.count(i)) row[i]++;
+                    if(!col.count(j)) col[j]++;
                 }
             }
         }
-        for (int j = 0; j < matrix[0].size(); j++) {
-            for (int i = 0; i < matrix.size(); i++) {
-                if (matrix[i][j] == 0) {
-                    col.push_back(j);
-                    break;
-                }
-            }
-        }
-        int k = 0;
+        
         for (int i = 0; i < matrix.size(); i++) {
-            if (k<row.size() && i == row[k]) {
+            if (row.count(i)) {
                 for (int j = 0; j < matrix[i].size(); j++) {
                     matrix[i][j] = 0;
                 }
-                k++;
             }
         }
 
-        k = 0;
 
         for (int j = 0; j < matrix[0].size(); j++) {
-            if (k<col.size() && col[k] == j) {
+            if (col.count(j)) {
                 for (int i = 0; i < matrix.size(); i++) {
                     matrix[i][j]=0;
                 }
-                k++;
             }
         }
     }
