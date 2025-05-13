6906b0561dd8916dd41ffb5edbcbf1b30518cce1
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-13 09:40:30 +0530

    Time: 0 ms (100%), Space: 17.7 MB (57.58%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int ans=INT_MIN;
-        for(int i=0;i<nums.size()-1;i++){
-            int curr=nums[i];
-            ans=max(ans,curr);
-            for(int j=i+1;j<nums.size();j++){
-                curr*=nums[j];
-                ans=max(ans,curr);
-            }
+
+        // Brute force
+        // int ans=INT_MIN;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     int curr=nums[i];
+        //     ans=max(ans,curr);
+        //     for(int j=i+1;j<nums.size();j++){
+        //         curr*=nums[j];
+        //         ans=max(ans,curr);
+        //     }
+        // }
+        // ans=max(ans,nums[nums.size()-1]);
+        // return ans;
+
+
+        // Kadan's product version
+        int curr=1,prod1=INT_MIN;
+        for(auto it:nums){
+            curr*=it;
+            prod1=max(prod1,curr);
+            if(curr==0) curr=1;
         }
-        ans=max(ans,nums[nums.size()-1]);
-        return ans;
+
+        int prod2=INT_MIN;
+        curr=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            curr*=nums[i];
+            prod2=max(prod2,curr);
+            if(curr==0) curr=1;
+        }
+        return max(prod1,prod2);
     }
 };
