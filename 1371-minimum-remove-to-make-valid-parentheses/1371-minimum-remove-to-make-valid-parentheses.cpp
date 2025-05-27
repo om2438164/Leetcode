85c4872105de4666dcfb7eea98f1caae52e98e6d
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-27 11:41:53 +0530

    Time: 11 ms (43.2%), Space: 14.6 MB (31.75%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,31 +1,76 @@
+// class Solution {
+// public:
+//     string minRemoveToMakeValid(string s) {
+
+        // Brute force solution 2 pass
+
+//         unordered_map<int,int>map;
+//         stack<vector<string>>st;
+//         for(int i=0;i<s.size();i++){
+//             if(st.empty() && s[i]==')'){
+//                 map[i]++;
+//                 continue;
+//             }
+//             if(s[i]=='('){
+//                 st.push({to_string(s[i]),to_string(i)});
+//                 continue;
+//             }
+//             if(s[i]==')'){
+//                 st.pop();
+//             }
+//         }
+//         while(!st.empty()){
+//             map[stoi(st.top()[1])]++;
+//             st.pop();
+//         }
+//         string ans="";
+//         for(int i=0;i<s.size();i++){
+//             if(!map.count(i)){
+//                 ans+=s[i];
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    string minRemoveToMakeValid(string s) {
-        unordered_map<int,int>map;
-        stack<vector<string>>st;
-        for(int i=0;i<s.size();i++){
-            if(st.empty() && s[i]==')'){
-                map[i]++;
+    std::string minRemoveToMakeValid(std::string s) {
+        int leftCount = 0;
+        int rightCount = 0;
+        std::stack<char> stack;
+
+        // Pass 1
+        for (char ch : s) {
+            if (ch == '(') {
+                leftCount++;
+            } else if (ch == ')') {
+                rightCount++;
+            }
+            if (rightCount > leftCount) {
+                rightCount--;
                 continue;
-            }
-            if(s[i]=='('){
-                st.push({to_string(s[i]),to_string(i)});
-                continue;
-            }
-            if(s[i]==')'){
-                st.pop();
+            } else {
+                stack.push(ch);
             }
         }
-        while(!st.empty()){
-            map[stoi(st.top()[1])]++;
-            st.pop();
-        }
-        string ans="";
-        for(int i=0;i<s.size();i++){
-            if(!map.count(i)){
-                ans+=s[i];
+
+        std::string result = "";
+        
+        // Pass 2
+        while (!stack.empty()) {
+            char currentChar = stack.top();
+            stack.pop();
+            if (leftCount > rightCount && currentChar == '(') {
+                leftCount--;
+            } else {
+                result += currentChar;
             }
         }
-        return ans;
+
+        // Reverse the result string
+        std::reverse(result.begin(), result.end());
+        return result;
     }
 };
