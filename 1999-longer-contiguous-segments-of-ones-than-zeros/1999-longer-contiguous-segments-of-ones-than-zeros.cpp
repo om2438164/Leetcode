a45f64a33a2d84620ee4704a5331d2af5d1ceb9d
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-05-28 09:22:26 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.51%) - LeetHub

--- 1999-longer-contiguous-segments-of-ones-than-zeros/1999-longer-contiguous-segments-of-ones-than-zeros.cpp
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/1999-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -3,26 +3,23 @@ public:
     bool checkZeroOnes(string s) {
         int onecount=0;
         int ones=0;
+        int zerocount=0;
+        int zeros=0;
         for(auto &it:s){
             if(it=='1') onecount++;
             else {
                 ones=max(ones,onecount);
                 onecount=0;
             }
-        }
-        ones=max(ones,onecount);
-        int zerocount=0;
-        int zeros=0;
-        for(auto &it:s){
             if(it=='0') zerocount++;
             else{
                 zeros=max(zeros,zerocount);
                 zerocount=0;
             } 
         }
+        ones=max(ones,onecount);
         zeros=max(zeros,zerocount);
-        cout<<zeros<<endl;
-        cout<<ones<<endl;
+        
         return (ones>zeros)?1:0;
     }
 };
