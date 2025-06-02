90111b7fb93406f5c7282307dc5fb64914b391cb
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-06-02 09:21:04 +0530

    Time: 3 ms (71.75%), Space: 16.4 MB (13.85%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(string op,int open,int close,vector<string>&ans){
+        if(open==0 && close ==0){
+            ans.push_back(op);
+            return;
+        }
+
+
+        if(open==close){
+            string op1=op+'(';
+            solve(op1,open-1,close,ans);
+        }else if(open==0){
+            string op1=op+')';
+            solve(op1,open,close-1,ans);
+        }else{
+            string op1=op+'(';
+            string op2=op+')';
+            solve(op1,open-1,close,ans);
+            solve(op2,open,close-1,ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        int open=n,colse=n;
+        string op="";
+        solve(op,n,n,ans);
+        return ans;
+    }
+};
