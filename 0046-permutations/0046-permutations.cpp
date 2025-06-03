3020fd12d962e6e67863ea8110cb420b79eb3595
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-06-03 22:07:48 +0530

    Time: 0 ms (100%), Space: 10.6 MB (69.68%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<int>temp,vector<vector<int>>&ans){
-        if(nums.size()==0){
-            ans.push_back(temp);
+    // void solve(vector<int>&nums,vector<int>temp,vector<vector<int>>&ans){
+    //     // if(nums.size()==0){
+    //     //     ans.push_back(temp);
+    //     //     return;
+    //     // }
+
+    //     // for(int i=0;i<nums.size();i++){
+    //     //     int element=nums[i];
+    //     //     temp.push_back(nums[i]);
+    //     //     nums.erase(nums.begin()+i);
+    //     //     solve(nums,temp,ans);
+    //     //     nums.insert(nums.begin()+i,element);
+    //     //     temp.pop_back();
+    //     // }
+
+
+    // }
+
+    void solve(vector<int>&nums,vector<vector<int>>&result,int &start){
+        if(start>=nums.size()){
+            result.push_back(nums);
             return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            int element=nums[i];
-            temp.push_back(nums[i]);
-            nums.erase(nums.begin()+i);
-            solve(nums,temp,ans);
-            nums.insert(nums.begin()+i,element);
-            temp.pop_back();
+        for(int i=start;i<nums.size();i++){
+            swap(nums[start],nums[i]);
+            start=start+1;
+            solve(nums,result,start);
+            start--;//backtrack
+            swap(nums[start],nums[i]);//backtrack
         }
-
-
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>result;
-        vector<int>temp;
+        // vector<vector<int>>result;
+        // vector<int>temp;
 
-        solve(nums,temp,result);
+        // solve(nums,temp,result);
+        // return result;
+
+
+        vector<vector<int>>result;
+        int start=0;
+        solve(nums,result,start);
         return result;
     }
 };
