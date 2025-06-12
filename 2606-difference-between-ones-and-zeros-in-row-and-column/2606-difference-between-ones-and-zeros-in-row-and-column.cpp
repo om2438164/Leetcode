d85d278b3445fc50b49cc1c5186a4cf3b0e94fe7
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-06-12 19:28:18 +0530

    Time: 100 ms (8.55%), Space: 148.3 MB (8.55%) - LeetHub

--- 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -4,26 +4,21 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>row(m,vector<int>(2));
-        
-        for(int i=0;i<m;i++){
-            int ones=0,zeros=0;
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0) zeros++;
-                if(grid[i][j]==1) ones++;
-            }
-            row[i]={zeros,ones};
-        }
-
         vector<vector<int>>col(n,vector<int>(2));
-
-        for(int j=0;j<n;j++){
-            int ones=0,zeros=0;
-            for(int i=0;i<m;i++){
-                if(grid[i][j]==0) zeros++;
-                if(grid[i][j]==1) ones++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    row[i][0]++;
+                    col[j][0]++;
+                    continue;
+                }
+                if(grid[i][j]==1){
+                    row[i][1]++;
+                    col[j][1]++;
+                } 
             }
-            col[j]={zeros,ones};
         }
+
         vector<vector<int>>ans(m,vector<int>(n));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
