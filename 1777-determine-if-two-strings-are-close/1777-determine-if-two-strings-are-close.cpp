3184ddbdba8a7031e0865517022ea84004b4d44b
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-06-27 22:15:32 +0530

    Time: 34 ms (34.37%), Space: 24.1 MB (10.44%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool closeStrings(string word1, string word2) {
         if(word1.size()!=word2.size()) return false;
-
         unordered_map<char,int>map1;
         unordered_map<char,int>map2;
-
         for(int i=0;i<word1.size();i++){
             map1[word1[i]]++;
             map2[word2[i]]++;
