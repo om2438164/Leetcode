c2895bc09a249bb38d3e1d8dfd94f1ebd120c302
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-07-26 23:04:27 +0530

    Time: 0 ms (100%), Space: 19.3 MB (14.01%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,19 +11,47 @@
  */
 class Solution {
 public:
-    int ans=0;
-    void preorder(TreeNode* node,int count){
-        if(node==NULL){
-            ans=max(ans,count);
-            return;
+    // 1. Using DFS 
+
+    // int ans=0;
+    // void preorder(TreeNode* node,int count){
+    //     if(node==NULL){
+    //         ans=max(ans,count);
+    //         return;
+    //     }
+    //     count++;
+    //     preorder(node->left,count);
+    //     preorder(node->right,count);
+    // }
+
+    // 2. Using BFS
+    void bfs(TreeNode* node,int& ans){
+        queue<TreeNode*>q;
+        q.push(node);
+        q.push(NULL);
+
+        while(!q.empty()){
+            TreeNode* front=q.front();
+            q.pop();
+            if(front==NULL){
+                ans++;
+                if(!q.empty()) q.push(NULL);
+
+            }else{
+                if(front->left!=NULL) q.push(front->left);
+                if(front->right!=NULL) q.push(front->right);
+            }
         }
-        count++;
-        preorder(node->left,count);
-        preorder(node->right,count);
     }
+
     int maxDepth(TreeNode* root) {
-        int count=0;
-        preorder(root,count);
+        // int count=0;
+        // preorder(root,count);
+        // return ans;
+
+        if(root==NULL) return 0;
+        int ans=0;
+        bfs(root,ans);
         return ans;
     }
 };
