3baaaba34c26cde0e4372e4613aed69eec837aa1
Om Patil <117569343+om2438164@users.noreply.github.com>
2025-08-09 21:31:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
-    bool ispalindrome(string &s){
-        int start=0,end=s.size()-1;
+    bool ispalindrome(string &s,int start,int end){
         while(start<=end){
             if(s[start]!=s[end]) return false;
             start++,end--;
         }
         return true;
     }
+
+    string solve(string &s,int i,int j){
+        if(i>j) return "";
+
+
+        // string temp=s;
+        // temp.erase(0,1);
+        // if(ispalindrome(temp)) return temp;
+        // string temp2=s;
+        // temp2.erase(temp2.size()-1,1);
+        if(ispalindrome(s,i,j)){
+            return s.substr(i,j-i+1);
+        } 
+
+
+        string a= solve(s,i+1,j);
+        string b= solve(s,i,j-1);
+
+        return (a.size()>b.size())? a:b;
+
+    }
     string longestPalindrome(string s) {
-        int maxi=INT_MIN;
-        string ans="";
-        for(int i=0;i<s.size();i++){
-            string temp="";
-            for(int j=i;j<s.size();j++){
-                temp+=s[j];
-                if((j-i+1)>=maxi && ispalindrome(temp)){
-                    ans=temp;
-                    maxi=j-i+1;
-                }
-            }
-        }
-        return ans;
+        // int maxi=INT_MIN;
+        // string ans="";
+        // for(int i=0;i<s.size();i++){
+        //     string temp="";
+        //     for(int j=i;j<s.size();j++){
+        //         temp+=s[j];
+        //         if((j-i+1)>=maxi && ispalindrome(temp)){
+        //             ans=temp;
+        //             maxi=j-i+1;
+        //         }
+        //     }
+        // }
+        // return ans;
+        return solve(s,0,s.size()-1);
     }
 };
